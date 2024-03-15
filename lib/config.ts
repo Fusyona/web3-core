4d9308c8a2bdda8088452a8e4e3522d3e490f245
Mr.PAPI <thesnakewitcher@gmail.com>
2024-03-14 21:00:43 -0400

    feat: add config.ts module
    These module includes the `NetworkConfig` class that represents
    the network config for a single chain and `NetworkConfigs` class
    which contains a network config per network in networks.json

---
+++ lib/config.ts
@@ -0,0 +1,105 @@
+import { vars } from "hardhat/config"
+import { HttpNetworkUserConfig, HttpNetworkAccountsUserConfig } from "hardhat/types"
+import { ChainConfig } from "@nomicfoundation/hardhat-verify/types" ;
+
+import { NetworkConfigData } from "./types"
+import networks from "./networks.json" ;
+
+
+class NetworkConfig {
+
+    public name: string ;
+    public rpcUrl: string ;
+    public chainId: number ;
+    public explorerUrl: string ;
+    public accounts: HttpNetworkAccountsUserConfig ;
+
+    constructor(networkName: string, networkData: NetworkConfigData, accounts: HttpNetworkAccountsUserConfig) {
+        this.name = networkName ;
+        this.rpcUrl = networkData.rpcUrl ;
+        this.chainId = networkData.chainId ;
+        this.explorerUrl = networkData.explorerUrl! ;
+        this.accounts = accounts ;
+    }
+
+    withAccounts(accounts: HttpNetworkAccountsUserConfig) {
+        this.accounts = accounts ;
+        return this ;
+    }
+
+    withApiKey(apiKeyName?: string) {
+        this.setApiKey(apiKeyName) ;
+        return this ;
+    }
+
+    setApiKey(apiKeyName?: string) {
+        if ( this.rpcUrlNeedsApiKey() ) {
+
+            if ( typeof apiKeyName === undefined ) {
+                const apiKey = vars.get(this.rpcUrlApiKeyName) ;
+                this.rpcUrl = this.rpcUrl.replace(
+                    this.rpcUrlApiKeyPattern,
+                    apiKey
+                )
+                return
+            }
+
+            const apiKey = vars.get(apiKeyName!)
+            this.rpcUrl = `${this.rpcUrl}/${apiKey}`
+        }
+    }
+
+    config(apiKey?: string) : HttpNetworkUserConfig {
+        return {
+            url: apiKey ? `${this.rpcUrl}/${apiKey}` : this.rpcUrl,
+            chainId: this.chainId,
+            accounts: this.accounts,
+        }
+    }
+
+    get explorerApiUrl() : string {
+        return `${this.explorerUrl}/api`
+    }
+
+    get customChain() : ChainConfig {
+        return {
+            network: this.name,
+            chainId: this.chainId,
+            urls: {
+                apiURL: this.explorerApiUrl,
+                browserURL: this.explorerUrl,
+            }
+        }
+    }
+
+    private rpcUrlNeedsApiKey() : boolean {
+        return this.rpcUrlApiKeyMatches !== null
+    }
+
+    get rpcUrlApiKeyName() : string {
+        return this.rpcUrlApiKeyMatches?.[1]!
+    }
+
+    get rpcUrlApiKeyPattern() : string {
+        return this.rpcUrlApiKeyMatches?.[0]!
+    }
+
+    get rpcUrlApiKeyMatches() : RegExpMatchArray | null {
+        return this.rpcUrl.match(/\{(.+)\}/)
+    }
+
+}
+
+export default class NetworkConfigs {
+
+    // @TODO assign type
+    public networks: any = {} ;
+
+    constructor(accounts: string[]) {
+        Object.entries(networks).map( ([name, data]) => {
+            const networkConfig = new NetworkConfig(name, data, accounts) ;
+            this.networks[name] = networkConfig.withApiKey() ;
+        })
+    }
+
+}
