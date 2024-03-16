54d674e7231aec71e707c62ea9c8de696e6463f5
MakeMake23 <andy.ledesma.garcia@gmail.com>
2024-03-16 00:49:04 -0400

    test: stop using fixture

--- test/Multisig.ts
+++ test/Multisig.ts
@@ -1,27 +1,27 @@
-import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { Multisig } from "../typechain-types";
 
-describe("Multisig", function() {
-    async function deployMultisigContract() {
+describe("Multisig", function () {
+    let signerAddresses: string[] = [];
+    let multisig: Multisig;
+
+    beforeEach(async () => {
         const signers = await ethers.getSigners();
 
-        let signerAddresses: string[] = [];
         // Using 3 wallets for initial test
-        for (let i  = 0; i < 3; i++) {
-            signerAddresses.push(await signers[i].getAddress())
+        for (let i = 0; i < 3; i++) {
+            signerAddresses.push(await signers[i].getAddress());
         }
-
         const Multisig = await ethers.getContractFactory("Multisig");
-        const multisig = await Multisig.deploy(signerAddresses);
-        
-        return { multisig, signers, signerAddresses };
-    }
+        multisig = (await Multisig.deploy(signerAddresses)) as Multisig;
+        await multisig.deployed();
+    });
 
-    describe("Deployment", function() {
-        it("Should return the correct signers array", async function() {
-            const { multisig, signerAddresses } = await loadFixture(deployMultisigContract);
-            expect(await multisig.signers()).to.equal(signerAddresses);
+    describe("Deployment", function () {
+        it("Should return the correct signers array", async function () {
+            for (let i = 0; i < 3; i++)
+                expect(await multisig.signers(i)).to.equal(signerAddresses[i]);
         });
-    })
-})
+    });
+});
