672b6d81814b7dfc3c9c397f71a74e0465841a25
Mr.PAPI <thesnakewitcher@gmail.com>
2024-03-27 12:47:18 -0400

    chore: add hardhat-inspect devDependency

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "hardhat-deploy"
-import "@nomiclabs/hardhat-ethers"
-import "hardhat-tracer"
+import "hardhat-deploy";
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-tracer";
+import "hardhat-inspect";
 
 const config: HardhatUserConfig = {
     solidity: "0.8.24",
