8f199c089643a9b42914445e6a3afab131bf80a4
Mr.PAPI <thesnakewitcher@gmail.com>
2024-03-27 12:51:23 -0400

    chore: add namedAccounts to hardhat.config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,8 +5,16 @@ import "@nomiclabs/hardhat-ethers";
 import "hardhat-tracer";
 import "hardhat-inspect";
 
+
 const config: HardhatUserConfig = {
     solidity: "0.8.24",
+    namedAccounts: {
+        deployer: {
+            default: "0x16c7C5849A6769d58F9df6A26960F3293EF379e0",
+            localhost: 0,
+            hardhat: 0,
+        }
+    }
 };
 
 export default config;
