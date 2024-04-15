9d1726a3c263d5b4e5fd12d965132c287791d009
adrianhdez929 <adrianhdez929@gmail.com>
2024-04-15 19:26:13 -0400

    build(node): update dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,7 +8,7 @@ import "hardhat-neovim"
 
 
 const config: HardhatUserConfig = {
-    solidity: "0.8.24",
+    solidity: "0.8.25",
     namedAccounts: {
         deployer: {
             default: "0x16c7C5849A6769d58F9df6A26960F3293EF379e0",
