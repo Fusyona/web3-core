ba6cdbc5d24359e1d2bf52899846d41f185f6850
adrianhdez929 <adrianhdez929@gmail.com>
2024-04-19 13:40:16 -0400

    ref: add implementation contract interface to use abi.encodeCall

--- contracts/Multisig_example.sol
+++ contracts/Multisig_example.sol
@@ -3,15 +3,29 @@ pragma solidity 0.8.25;
 
 import "./Multisig.sol";
 
+interface IMSExample {
+    function sumTwoNumbers(uint256 x, uint256 y) external returns (uint256 result);
+    function modifierHelloWorld() external returns (string memory result);
+}
+
 contract MSExample is Multisig {
     constructor(address[] memory signers, uint256 signCount) Multisig(signers, signCount) {}
 
     function modifierHelloWorld() 
-        public 
+        external 
         onlySigner
-        useMultisig(abi.encodeWithSignature("modifierHelloWorld()")) 
+        useMultisig(abi.encodeCall(IMSExample.modifierHelloWorld, ())) 
         returns (string memory result) 
     {
         result = "Hello World";
+    }
+
+    function sumTwoNumbers(uint256 x, uint256 y) 
+        external
+        onlySigner
+        useMultisig(abi.encodeCall(IMSExample.sumTwoNumbers, (x, y)))
+        returns (uint256 result) 
+    {
+        result = x + y;   
     }
 }
