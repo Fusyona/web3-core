79b518eed6327a86e52ae75fdb46422996a0bbe4
Mr.PAPI <thesnakewitcher@gmail.com>
2024-04-20 09:32:19 -0400

    feat: add ERC721MintScheme base component

---
+++ contracts/components/ERC721MintScheme.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19 ;
+pragma abicoder v2 ;
+
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol" ;
+
+abstract contract ERC721MintScheme is ERC721 {
+
+    function mint(address receiver) internal virtual {
+        uint256 tokenId = _getNextTokenId() ;
+        _checkTokenId(tokenId);
+        _safeMint(receiver, tokenId) ;
+    }
+
+    function _getNextTokenId() internal virtual returns(uint256 tokenId) ;
+
+    function _checkTokenId(uint256 tokenId) internal virtual {}
+
+}
