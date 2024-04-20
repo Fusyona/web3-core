4376481fa67a3c195908fa38fb7e63e83729f5db
Mr.PAPI <thesnakewitcher@gmail.com>
2024-04-20 09:32:59 -0400

    feat: add ERC721MintSchemeCounterCaped component

---
+++ contracts/components/ERC721MintSchemeCounterCaped.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19 ;
+pragma abicoder v2 ;
+
+
+import "@openzeppelin/contracts/utils/Counters.sol" ;
+import "./ERC721MintSchemeCounter.sol" ;
+
+
+abstract contract ERC721MintSchemeCounterCaped is ERC721MintSchemeCounter {
+    
+    error CapExceeded() ;
+
+    uint256 cap ;
+
+    function _checkTokenId(uint256 tokenId) internal virtual override {
+        if (tokenId > cap ) revert CapExceeded() ;
+    }
+
+}
