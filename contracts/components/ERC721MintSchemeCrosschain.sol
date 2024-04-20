940a5607dcf069a381e04b15b23f3ecac711b360
Mr.PAPI <thesnakewitcher@gmail.com>
2024-04-20 10:33:44 -0400

    fix: remove ++tokenIdCounter in ERC721MintSchemeCrosschain

--- contracts/components/ERC721MintSchemeCrosschain.sol
+++ contracts/components/ERC721MintSchemeCrosschain.sol
@@ -8,14 +8,12 @@ import { ERC721MintSchemeCounter } from "./ERC721MintSchemeCounter.sol" ;
 
 abstract contract ERC721MintSchemeCrosschain is ERC721MintSchemeCounter {
 
-    function _getNextTokenId() internal virtual override returns(uint256 tokenId) {
-        tokenId = uint256(keccak256(bytes.concat(
+    function _getNextTokenId() internal virtual override returns(uint256) {
+        return uint256(keccak256(bytes.concat(
             bytes32(super._getNextTokenId()),
-            bytes32(block.prevrandao),
             bytes32(block.chainid),
-            bytes32(tokensIdCounter)
+            bytes32(block.prevrandao)
         ))) ;
-        ++tokensIdCounter ;
     }
 
 }
