47ebdb7b99414570beaf3e7baba62921d548c7ce
Mr.PAPI <thesnakewitcher@gmail.com>
2024-04-20 09:32:37 -0400

    feat: add ERC721MintSchemeCounter component

---
+++ contracts/components/ERC721MintSchemeCounter.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19 ;
+pragma abicoder v2 ;
+
+
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol" ;
+import { ERC721MintScheme } from "./ERC721MintScheme.sol" ;
+
+
+abstract contract ERC721MintSchemeCounter is ERC721MintScheme {
+
+    uint256 internal tokensIdCounter ;
+
+    function _getNextTokenId() internal virtual override returns(uint256 tokenId) {
+        return ++tokensIdCounter ;
+    }
+
+}
