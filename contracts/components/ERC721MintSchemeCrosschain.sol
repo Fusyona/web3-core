89bc924c1c14f56af85bbba05247f7bacfc5a4df
Mr.PAPI <thesnakewitcher@gmail.com>
2024-04-22 16:52:40 -0400

    ref: add block.timestamp to _getNextTokenId() in ERC721MintSchemeCrosschain

--- contracts/components/ERC721MintSchemeCrosschain.sol
+++ contracts/components/ERC721MintSchemeCrosschain.sol
@@ -8,10 +8,15 @@ import { ERC721MintSchemeCounter } from "./ERC721MintSchemeCounter.sol" ;
 
 abstract contract ERC721MintSchemeCrosschain is ERC721MintSchemeCounter {
 
+    /**
+     * @dev These isn't safe randomness, what we are looking for here is a collision
+     *      resistant id even if it  could be know ahead of time
+     */
     function _getNextTokenId() internal virtual override returns(uint256) {
         return uint256(keccak256(bytes.concat(
             bytes32(super._getNextTokenId()),
             bytes32(block.chainid),
+            bytes32(block.timestamp),
             bytes32(block.prevrandao)
         ))) ;
     }
