00b18f0960233482906238f83cc47a5c067f0b13
adrianhdez929 <adrianhdez929@gmail.com>
2024-04-30 10:26:26 -0400

    fix: fix invalid references in multicall function

--- contracts/Multicall.sol
+++ contracts/Multicall.sol
@@ -4,20 +4,20 @@ pragma solidity 0.8.25;
 import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 import { Context } from "@openzeppelin/contracts/utils/Context.sol";
 
-type CallQuery {
+struct CallQuery {
     address target;
     bytes data;
 }
 
-abstract contract MulticallProxy {
+abstract contract MulticallProxy is Context {
     function multicall(CallQuery[] calldata calls) external virtual returns (bytes[] memory results) {
         bytes memory context = msg.sender == _msgSender()
             ? new bytes(0)
             : msg.data[msg.data.length - _contextSuffixLength():];
 
-            results = new bytes[](data.length);
-            for (uint256 i; i < data.length; ++i) {
-                results[i] = Address.functionDelegateCall(calls[i].target, bytes.concat(data[i], context));
+            results = new bytes[](calls.length);
+            for (uint256 i; i < calls.length; ++i) {
+                results[i] = Address.functionDelegateCall(calls[i].target, bytes.concat(calls[i].data, context));
             }
 
             return results;
