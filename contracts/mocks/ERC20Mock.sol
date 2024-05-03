77b92eae81f451d97a5c8e004eaadca6adc76d22
adrianhdez929 <adrianhdez929@gmail.com>
2024-05-03 13:28:05 -0400

    ref: add mint to mock token constructor

--- contracts/mocks/ERC20Mock.sol
+++ contracts/mocks/ERC20Mock.sol
@@ -4,10 +4,8 @@ pragma solidity 0.8.25;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
-contract MockTokenA is ERC20 {
-    constructor() ERC20("TokenA", "TKA") {}
-}
-
-contract MockTokenB is ERC20 {
-    constructor() ERC20("TokenB", "TKB") {}
+contract ERC20Mock is ERC20 {
+    constructor(uint256 amount, string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, amount);
+    }
 }
