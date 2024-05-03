8e01d0c56daf9874a1a50830803b7cb0572d7899
adrianhdez929 <adrianhdez929@gmail.com>
2024-05-03 15:01:24 -0400

    ref: change mock implementatio to couple with test

--- contracts/mocks/ERC20Mock.sol
+++ contracts/mocks/ERC20Mock.sol
@@ -5,7 +5,7 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
 contract ERC20Mock is ERC20 {
-    constructor(uint256 amount, string memory name, string memory symbol) ERC20(name, symbol) {
-        _mint(msg.sender, amount);
+    constructor(address to, uint256 amount, string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(to, amount);
     }
 }
