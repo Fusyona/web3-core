c7981315bb1a8c1d822b6f43ee5a880950f0c629
adrianhdez929 <adrianhdez929@gmail.com>
2024-06-08 23:09:00 -0400

    feat: add Erc20 mock contract

---
+++ contracts/mocks/ERC20Mock.sol
@@ -0,0 +1,12 @@
+
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.25;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract ERC20Mock is ERC20 {
+    constructor(address to, uint256 amount, string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(to, amount);
+    }
+}
