b6c571dda7a093c91c643307fd950048334f3109
adrianhdez929 <adrianhdez929@gmail.com>
2024-06-10 12:01:22 -0400

    test: make test more atomic, and remove redundant assertions

--- test/Wrappers/wrapper-erc20.test.ts
+++ test/Wrappers/wrapper-erc20.test.ts
@@ -18,28 +18,23 @@ describe("ERC20 Wrapper", async () => {
     })
 
     it("should show token symbol", async () => {
-        expect(await tokenWrapper.symbol())
+        expect(await tokenWrapper.call.symbol())
             .to.be.equal("MKT")
     })
     it("should show token name", async () => {
-        expect(await tokenWrapper.name())
+        expect(await tokenWrapper.call.name())
             .to.be.equal("Mock")
     })
     it("should show token decimals", async () => {
-        expect(await tokenWrapper.decimals())
+        expect(await tokenWrapper.call.decimals())
             .to.be.equal("18")
     })
     it("should show token totalSupply", async () => {
-        expect(await tokenWrapper.totalSupply())
+        expect(await tokenWrapper.call.totalSupply())
             .to.be.equal("100")
     })
     it("should transfer", async () => {
         const [deployer, target] = await ethers.getSigners()
-
-        expect(await tokenWrapper.balanceOf(await deployer.getAddress()))
-            .to.be.equal("100")
-        expect(await tokenWrapper.balanceOf(await target.getAddress()))
-            .to.be.equal("0")
 
         expect(await tokenWrapper.withSigner(deployer).transfer(await target.getAddress(), "10"))
             .to.changeTokenBalance(tokenContract, [deployer, target], [-10, 10])
@@ -47,17 +42,7 @@ describe("ERC20 Wrapper", async () => {
     it("should approve and transferFrom", async () => {
         const [deployer, target, spender] = await ethers.getSigners()
 
-        expect(await tokenWrapper.balanceOf(await deployer.getAddress()))
-            .to.be.equal("100")
-        expect(await tokenWrapper.balanceOf(await target.getAddress()))
-            .to.be.equal("0")
-
         await tokenWrapper.withSigner(deployer).approve(await spender.getAddress(), "10")
-
-        expect(await tokenWrapper.allowance(await deployer.getAddress(), await spender.getAddress()))
-            .to.be.equal("10")
-
-        
 
         expect(await tokenWrapper.withSigner(spender).transferFrom(await deployer.getAddress(), await target.getAddress(), "10"))
             .to.changeTokenBalance(tokenContract, [deployer, target], [-10, 10])
--- test/Wrappers/wrapper-erc721.test.ts
+++ test/Wrappers/wrapper-erc721.test.ts
@@ -18,55 +18,37 @@ describe("ERC721 Wrapper", async () => {
     })
 
     it("should show token symbol", async () => {
-        expect(await tokenWrapper.symbol())
+        expect(await tokenWrapper.call.symbol())
             .to.be.equal("MKT")
     })
     it("should show token name", async () => {
-        expect(await tokenWrapper.name())
+        expect(await tokenWrapper.call.name())
             .to.be.equal("Mock")
     })
     it("Should show tokenUri", async () => {
-        expect(await tokenWrapper.tokenUri('1'))
+        expect(await tokenWrapper.call.tokenURI('1'))
             .to.be.equal("")
     })
     it("should transferFrom", async () => {
         const [deployer, target] = await ethers.getSigners()
 
-        expect(await tokenWrapper.balanceOf(await deployer.getAddress()))
-            .to.be.equal("1")
-        expect(await tokenWrapper.balanceOf(await target.getAddress()))
-            .to.be.equal("0")
-
         expect(await tokenWrapper.withSigner(deployer).transferFrom(await deployer.getAddress(), await target.getAddress(), "1"))
             .to.changeTokenBalance(tokenContract, [deployer, target], [-1, 1])
     })
-    it("should approve and approveAll and transferFrom", async () => {
+    it("should approve and transferFrom", async () => {
         const [deployer, target, spender] = await ethers.getSigners()
 
         await tokenWrapper.withSigner(deployer).approve(await spender.getAddress(), "1")
 
-        expect(await tokenWrapper.getApproved("1"))
-            .to.be.equal(await spender.getAddress())
-
-        expect(await tokenWrapper.balanceOf(await deployer.getAddress()))
-            .to.be.equal("1")
-        expect(await tokenWrapper.balanceOf(await target.getAddress()))
-            .to.be.equal("0")
-
         expect(await tokenWrapper.withSigner(spender).transferFrom(await deployer.getAddress(), await target.getAddress(), "1"))
-            .to.changeTokenBalance(tokenContract, [deployer, target], [-1, 1])
-        expect(await tokenWrapper.ownerOf("1"))
-            .to.be.equal(await target.getAddress())
+            .to.changeTokenBalance(tokenContract, [deployer, target], [-1, 1])        
+    })
+    it("should setApprovalForAll and transferFrom", async () => {
+        const [deployer, target, spender] = await ethers.getSigners()
 
         await tokenWrapper.withSigner(target).setApprovalForAll(await spender.getAddress(), true)
 
-        expect(await tokenWrapper.isApprovedForAll(await target.getAddress(), await spender.getAddress()))
-
-        
-
         expect(await tokenWrapper.withSigner(spender).transferFrom(await target.getAddress(), await deployer.getAddress(), "1"))
             .to.changeTokenBalance(tokenContract, [target, deployer], [-1, 1])
-        expect(await tokenWrapper.ownerOf("1"))
-            .to.be.equal(await deployer.getAddress())
     })
 })
