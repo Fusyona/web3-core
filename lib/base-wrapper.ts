4a97bc66dfc81507b72ebf493b1a2e69a5bd0253
MakeMake23 <andy.ledesma.garcia@gmail.com>
2024-08-27 09:24:37 -0400

    ref: stop requiring a provider in BaseWrapper

--- lib/base-wrapper.ts
+++ lib/base-wrapper.ts
@@ -1,16 +1,13 @@
-import { Signer, Contract, Transaction, AddressLike, Addressable, ContractTransactionResponse } from "ethers";
+import { Signer, Contract, AddressLike, Addressable, ContractTransactionResponse } from "ethers";
 
-import { Address, SupportedProvider } from "./types";
-import { waitAndReturn } from "./util";
+import { Address } from "./types";
 
 export default abstract class BaseWrapper implements Addressable {
     protected contract!: Contract;
-    protected provider!: SupportedProvider;
     protected signer: Signer | undefined;
     protected confirmations: number | undefined;
 
-    constructor(contract: Contract, provider: SupportedProvider, confirmations: number = 1) {
-        this.setProvider(provider);
+    constructor(contract: Contract, confirmations: number = 1) {
         this.setContract(contract);
         this.confirmations = confirmations;
     }
@@ -22,20 +19,6 @@ export default abstract class BaseWrapper implements Addressable {
 
     setContract(contract: Contract) {
         this.contract = contract;
-    }
-
-    withProvider(provider: SupportedProvider) {
-        this.setProvider(provider);
-        return this;
-    }
-
-    private setProvider(provider: SupportedProvider) {
-        this.provider = provider;
-    }
-
-    async withSignerIndex(index: number) {
-        const signer = await this.provider.getSigner(index);
-        return this.withSigner(signer);
     }
 
     // https://github.com/OpenZeppelin/openzeppelin-upgrades/blob/2ef7aa554c3b31821a79a99131751fb07b5b0298/packages/plugin-hardhat/src/utils/ethers.ts#L6-L8
@@ -55,7 +38,7 @@ export default abstract class BaseWrapper implements Addressable {
 
     protected async waitAndReturn(transactionPromise: Promise<ContractTransactionResponse>) {
         const transaction = await transactionPromise;
-        await this.provider.waitForTransaction(transaction.hash as string, this.confirmations);
+        await transaction.wait(this.confirmations);
         return transaction;
     }
 }
