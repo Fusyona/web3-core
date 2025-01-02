053892d9fe486508c9dfbf63c92be5d5cc082c64
Andy Ledesma García <62294636+MakeMake23@users.noreply.github.com>
2025-01-02 22:49:42 +0400

    build(ts): make pnpm tsc run successfully

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,10 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomiclabs/hardhat-solhint"
+import "@nomicfoundation/hardhat-ethers";
 import "hardhat-deploy";
 import "hardhat-deploy-ethers";
-import "hardhat-contract-sizer"
 import "hardhat-inspect";
 import "hardhat-neovim";
-
 
 const config: HardhatUserConfig = {
     solidity: "0.8.25",
@@ -15,8 +13,8 @@ const config: HardhatUserConfig = {
             default: "0x16c7C5849A6769d58F9df6A26960F3293EF379e0",
             localhost: 0,
             hardhat: 0,
-        }
-    }
+        },
+    },
 };
 
 export default config;
--- test/Multisig.ts
+++ test/Multisig.ts
@@ -1,86 +1,75 @@
 import { expect } from "chai";
-import {ethers, deployments, getUnnamedAccounts} from "hardhat";
-import { abi as MSExampleAbi } from "../artifacts/contracts/mocks/MultisigExample.sol/MSExample.json" ;
-import { MSExample } from "../typechain-types" ;
+import { ethers, deployments, getUnnamedAccounts } from "hardhat";
+import { MSExample } from "../typechain-types";
 
 describe("Multisig", async function () {
     const signCount = 4;
 
     const setup = deployments.createFixture(async () => {
-        await deployments.fixture("MSExample")
+        await deployments.fixture("MSExample");
 
-        const signerAddresses = await getUnnamedAccounts()
-        let signers: ethers.Signer[] = []
+        const signerAddresses = await getUnnamedAccounts();
+        let signers: ethers.Signer[] = [];
 
         for (let i = 0; i < signerAddresses.length; i++) {
-            signers.push(await ethers.getSigner(signerAddresses[i]))
+            signers.push(await ethers.getSigner(signerAddresses[i]));
         }
-        const multisigExampleContract = await ethers.getContract<MSExample>("MSExample")
+        const multisigExampleContract = await ethers.getContract<MSExample>("MSExample");
 
         return {
             multisigExampleContract,
-            signers
-        }
-    })
+            signers,
+        };
+    });
 
     describe("Deploy", function () {
         it("Should return the correct signers array", async function () {
-            const {signers, multisigExampleContract} = await setup()
+            const { signers, multisigExampleContract } = await setup();
 
-            for (let i = 0; i < 2*signCount; i++)
-                expect(await multisigExampleContract.signers(i)).to
-                    .equal(signers[i].address);
+            for (let i = 0; i < 2 * signCount; i++)
+                expect(await multisigExampleContract.signers(i)).to.equal(signers[i].address);
         });
-    })
+    });
 
-    describe("Modifier sign", function() {
-        it("Should not sign twice", async function() {
-            const {signers, multisigExampleContract} = await setup();
+    describe("Modifier sign", function () {
+        it("Should not sign twice", async function () {
+            const { signers, multisigExampleContract } = await setup();
 
-            expect(await multisigExampleContract.connect(signers[0]).modifierHelloWorld())
-                .to.not.be.reverted;
-            await expect(multisigExampleContract.connect(signers[0]).modifierHelloWorld())
-                .to.be.revertedWithCustomError(
-                    multisigExampleContract, 
-                    "AlreadySignedCall"
-                );
-        })
-        it("Should revert invalid signer", async function() {
-            const {signers, multisigExampleContract} = await setup();
+            expect(await multisigExampleContract.connect(signers[0]).modifierHelloWorld()).to.not.be.reverted;
+            await expect(
+                multisigExampleContract.connect(signers[0]).modifierHelloWorld(),
+            ).to.be.revertedWithCustomError(multisigExampleContract, "AlreadySignedCall");
+        });
+        it("Should revert invalid signer", async function () {
+            const { signers, multisigExampleContract } = await setup();
 
-            await expect(multisigExampleContract.connect(signers[2*signCount]).modifierHelloWorld())
-                .to.be.revertedWithCustomError(
-                    multisigExampleContract, 
-                    "InvalidSigner"
-                );
-        })
-        it("Should emit CallExecuted event", async function() {
-            const {signers, multisigExampleContract} = await setup();
-
-            for (let i = 0; i < signCount - 1; i++) {
-                expect(await multisigExampleContract.connect(signers[i]).modifierHelloWorld())
-                .to.not.be.reverted;
-            }
-
-            expect( await multisigExampleContract.connect(signers[signCount - 1]).modifierHelloWorld())
-            .to.emit(
-                multisigExampleContract,
-                "CallExecuted(bytes32)"
-            );
-        })
-        it("Should emut CallExecuted on multi param call", async function() {
+            await expect(
+                multisigExampleContract.connect(signers[2 * signCount]).modifierHelloWorld(),
+            ).to.be.revertedWithCustomError(multisigExampleContract, "InvalidSigner");
+        });
+        it("Should emit CallExecuted event", async function () {
             const { signers, multisigExampleContract } = await setup();
 
             for (let i = 0; i < signCount - 1; i++) {
-                expect(await multisigExampleContract.connect(signers[i]).sumTwoNumbers(2, 2))
-                .to.not.be.reverted;
+                expect(await multisigExampleContract.connect(signers[i]).modifierHelloWorld()).to.not.be.reverted;
             }
-            
-            expect(await multisigExampleContract.connect(signers[signCount - 1]).sumTwoNumbers(2, 2))
-            .to.emit(
+
+            expect(await multisigExampleContract.connect(signers[signCount - 1]).modifierHelloWorld()).to.emit(
                 multisigExampleContract,
-                "CallExecuted(bytes32)"
+                "CallExecuted(bytes32)",
             );
-        })
-    })
+        });
+        it("Should emut CallExecuted on multi param call", async function () {
+            const { signers, multisigExampleContract } = await setup();
+
+            for (let i = 0; i < signCount - 1; i++) {
+                expect(await multisigExampleContract.connect(signers[i]).sumTwoNumbers(2, 2)).to.not.be.reverted;
+            }
+
+            expect(await multisigExampleContract.connect(signers[signCount - 1]).sumTwoNumbers(2, 2)).to.emit(
+                multisigExampleContract,
+                "CallExecuted(bytes32)",
+            );
+        });
+    });
 });
