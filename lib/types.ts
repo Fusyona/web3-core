a4d6458b1001a0dad6b32983fa82ebb95bf372b7
MakeMake23 <andy.ledesma.garcia@gmail.com>
2025-01-19 13:48:55 +0400

    feat: implement multicall

--- lib/types.ts
+++ lib/types.ts
@@ -1,33 +1,29 @@
-import {
-    BrowserProvider,
-    JsonRpcProvider,
-    Addressable,
-    ContractTransaction,
-} from "ethers"
-import networks from "./networks"
+import { BrowserProvider, JsonRpcProvider, Addressable, ContractTransaction } from "ethers";
+import networks from "./networks";
 
+export const NATIVE_COIN = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
+export type Address = string;
+export type AddressOrAddressable = Address | Addressable;
 
-export const NATIVE_COIN = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
-export type Address = string
-export type AddressOrAddressable = Address | Addressable
+export type SupportedProvider = BrowserProvider | JsonRpcProvider;
+export type SupportedNetworkName = keyof typeof networks;
 
-export type SupportedProvider = BrowserProvider | JsonRpcProvider
-export type SupportedNetworkName = keyof typeof networks
+export type SupportedChainId = (typeof networks)[keyof typeof networks]["chainId"];
 
 export interface NetworkConfigData {
-    rpcUrl: string
-    chainId: number
-    explorerUrl?: string
+    rpcUrl: string;
+    chainId: number;
+    explorerUrl?: string;
 }
 
 export type NonTransactionFunctions<T> = Pick<
     T,
     {
-        [K in keyof T]: Exclude<T[K], (...args: any[]) => Promise<ContractTransaction>> extends never ? never : K
+        [K in keyof T]: Exclude<T[K], (...args: any[]) => Promise<ContractTransaction>> extends never ? never : K;
     }[keyof T]
 >;
 
 export namespace errors {
-    export const NoSigner = new Error("no signer connected")
-    export const NoProvider = new Error("no provider connected")
+    export const NoSigner = new Error("no signer connected");
+    export const NoProvider = new Error("no provider connected");
 }
